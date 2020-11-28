2a3bf3299be8b544885c8989454478d7cd5ebf1c
Shen Jun Wu <shenfive@gmail.com>
2020-11-28 11:50:55 +0800

    init

--- a211TableView/ViewController.swift
+++ a211TableView/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+
+    @IBOutlet weak var theTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        theTableView.dataSource = self
+        theTableView.delegate = self
     }
 
 
+    //Mark:TableView DataSource & Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.red
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
 }
 
